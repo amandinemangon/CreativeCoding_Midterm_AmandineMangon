a977324e35155a13712a43f06b8b5acceff1ca5b
amandinemangon <amandineemangon@gmail.com>
2025-11-14 21:24:11 -0500

    push
    finishing touches

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,17 +1,17 @@
 let lcurtainW = 355;
 let rcurtainW = 355;
+let carX = 0;
 
-let lightpink;
 let curtains;
-let darkpink;
+let darkgrey;
 let hair;
 let skin;
 let shirt;
 let floorcolor;
-let sky;
+let currentSky;
 let rain;
 
-let irisY = 111;
+let irisY = 111 ;
 
 let curtainsOpen = false;
 
@@ -20,13 +20,11 @@ let raindrops = [];
 function setup() {
   createCanvas(900, 500);
   
-  lightpink = color(204, 200, 202);
-  darkpink = color(168, 163, 166);
+  darkgrey = color(168, 163, 166);
   hair = color (61, 37, 2);
   floorcolor = color (133, 121, 102);
   shirt = color(64, 52, 58);
   skin = color (230, 198, 163);
-  sky = color (213, 225, 237);
   rain = color (13, 45, 115, 100);
   curtains = color(204, 200, 202, 200);
 
@@ -37,19 +35,39 @@ function setup() {
 }
 
 function draw() {
-  print(mouseX, mouseY);
+
   if (!curtainsOpen){
+    frameRate(60);
     scene1();
+    
   } else if (irisY < 115) {
+    frameRate(5);
     scene2();
-  } else if (millis() < 15000) {
+    
+  } else if (millis() < 30000) {
+    frameRate(60);
     scene3();
-  } else {
+    
+  } else if (carX === 0 && (leftPupilX > 463 || rightPupilX > 525)) {
+    frameRate(5);
     scene4();
+
+  } else if (carX < width) {
+    frameRate(20);
+    scene5();
+
+  } else if (leftPupilX < 483 || rightPupilX <545){
+    frameRate(5);
+    scene6();
+    
+  } else {
+    frameRate(60);
+    scene7();
   }
 }
 
 function scene1(){
+  currentSky = color(213, 225, 237);
   background(242, 237, 218);
   windows();
   bed();
@@ -59,7 +77,7 @@ function scene1(){
 }
 
 function windows(){
-  fill(sky);
+  fill(currentSky);
   stroke(184, 176, 149);
   strokeWeight(6);
   rect(100, 20, 375, 250);
@@ -87,18 +105,18 @@ function bed(){
   noStroke();
   rect(350, 350, 550, 150);
   
-  fill(lightpink);//duvet
-  stroke(darkpink);
+  fill(255);//duvet
+  stroke(darkgrey);
   rect(350, 350, 440, 150);
 
-  stroke(darkpink);//stripes on bed
+  stroke(darkgrey);//stripes on bed
   strokeWeight(6);
   for (let x = 350; x <= 800; x += 20) {
     line(x, 350, x, 550);
   }
   
   //pillows
-  fill(darkpink);
+  fill(darkgrey);
   noStroke();
   beginShape();
   vertex(850, 410);
@@ -144,10 +162,10 @@ function nightstand(){
 
 function clock(){
   fill(255);
-  stroke(darkpink);
+  stroke(darkgrey);
   strokeWeight(3);
   ellipse (150, 320, 40, 40);//main circle
-  fill(darkpink);
+  fill(darkgrey);
   ellipse(150, 295, 5, 5);//top part
   line(133, 331,128, 340);//outer lines
   line(167, 331, 172, 340);
@@ -163,23 +181,23 @@ function clock(){
 }
 
 function scene2(){
+  currentSky = color (125, 132, 138);
   background(floorcolor);
-  frameRate(5);
 
   fill(255);//bed
   noStroke();
   rect(0, 0, 750, 500);
   
   noStroke();//pillows
-  fill(darkpink);
+  fill(darkgrey);
   rect(75, 45, 250, 195);
   rect(400, 45, 250, 195);
   
   fill(shirt);
   ellipse(202, 247, 100, 180);
   
-  fill(lightpink);//duvet
-  stroke(darkpink);
+  fill(255);//duvet
+  stroke(darkgrey);
   strokeWeight(6);
   rect(0, height / 2, 750, 255);
 
@@ -306,10 +324,64 @@ class Raindrop{
 
 function scene4(){
   background(242, 237, 218);
+  door();
   Person3();
-  
 }
 
+function scene5(){
+  background(85, 90, 94);
+  fill(200);//road
+  noStroke();
+  rect(0, 300, 900, 200);
+  
+  //car
+  fill(255);
+  noStroke();
+  rect(carX, 250, 100, 70);
+
+  fill(0);
+  noStroke();
+  ellipse(carX + 20, 320, 20, 20);
+  ellipse(carX + 80, 320, 20, 20);
+  
+  if (carX < width) {
+    carX += 10;
+  }
+}
+
+function scene6(){
+  background(242, 237, 218);
+  door();
+  Person4();
+}
+
+function scene7(){
+  background(242, 237, 218);
+  currentSky = color(213, 225, 237);
+
+  fill(currentSky);//windowns
+  stroke(184, 176, 149);
+  strokeWeight(6);
+  rect(100, 20, 375, 250);
+  rect(480, 20, 375, 250);
+  
+  fill(255);
+  noStroke();
+  rect(80, 20, 795, 5);
+
+  fill(curtains);
+  noStroke();
+  rect(110, 20, lcurtainW, 245);
+  rect(845 - rcurtainW, 20, rcurtainW, 245);
+  
+  if (lcurtainW < 355) lcurtainW += 3;
+  if (rcurtainW < 355) rcurtainW += 3;
+  
+  bed();
+  nightstand();
+  clock();
+  Person1(810, 365);
+}
 
 let leftPupilX = 473;
 let rightPupilX = 535;
@@ -360,4 +432,62 @@ function Person3(){
   fill(shirt);
   noStroke();
   ellipse(510, 470, 280, 400);
+}
+
+
+function Person4(){
+  let x = width/2;
+  let y = height/2 + 250;
+  
+  fill(hair);
+  noStroke();
+  beginShape();
+  curveVertex(x - 50, y - 110);
+  curveVertex(x - 40, y - 425);
+  curveVertex(x + 60, y - 490);
+  curveVertex(x + 160, y - 425);
+  curveVertex(x + 175, y - 125);
+  curveVertex(x + 100, y - 100);
+  curveVertex(x - 50, y - 110);
+  curveVertex(x - 40, y - 425);
+  endShape(CLOSE);
+  
+  fill(skin);
+  noStroke();
+  ellipse(505, 170, 150, 200);
+  
+  fill(207, 178, 147);//eyelids
+  noStroke();
+  ellipse (473, 143, 25, 20);//left
+  ellipse (535, 143, 25, 20);//right
+  
+  fill(255);
+  stroke(0);
+  strokeWeight(0.5);//eyes
+  ellipse (473, 146, 25, 17);//left
+  ellipse(535, 146, 25, 17);//right
+  
+  if (leftPupilX < 483) {
+    leftPupilX += 0.3;
+  }
+  if (rightPupilX < 545) {
+    rightPupilX += 0.3;
+  }
+
+  fill(0);//pupil
+  ellipse(leftPupilX, 146, 10, 10);//left
+  ellipse(rightPupilX, 146, 10, 10);//right
+  
+  fill(shirt);
+  noStroke();
+  ellipse(510, 470, 280, 400);
+}
+
+function door(){
+  fill(255);
+  noStroke();
+  rect(640, 100, 200, 400);
+  fill(240);
+  stroke(0);
+  ellipse(800, 350, 20, 20);
 }
